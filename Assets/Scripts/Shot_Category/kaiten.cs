a81e27e89fa82b9fa86de8819288b20f79a5208b
ryoma0655 <ryouma9314@gmail.com>
2017-09-01 00:09:19 +0900

    aa

---
+++ Assets/Scripts/Shot_Category/kaiten.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class kaiten : MonoBehaviour {
+	public float z=10;
+	public float angle = 10;
+	public GameObject Shot;
+	void Start () {
+			Instantiate (Shot, transform.position, transform.rotation);
+	}
+		
+	// Startメソッドをコルーチンとして呼び出す
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Rotate(new Vector3(0, 0, 90) * Time.deltaTime, Space.World);
+	}
+}
